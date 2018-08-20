779c62ce202d18b207d27b6b6a6e44730caa45e9
Aryna Kandrashevich <Aryna_Kandrashevich@epam.com>
2018-08-20 17:20:55 +0300

    Upload task3

---
+++ Task3/ddl script.sql
@@ -0,0 +1,14 @@
+create table data 
+(PaymentID VARCHAR2(50),
+CustomerId VARCHAR2(50),
+ProductId VARCHAR2(50),
+TransactionDate VARCHAR2(50),
+CreditCard VARCHAR2(50),
+CreditCardNumber VARCHAR2(50)
+);
+
+
+CREATE TABLE FILE_NAMES
+(
+FILENAME VARCHAR2(50)
+);
